ad86c4912ef6a99a176ceabd6a9f7acc83e3f95a
tvanbesi <tvanbesi@proton.me>
2022-09-10 13:43:16 +0200

    Edit libft Makefile and libft.h
    The libft was not compiling the linked list functions in the library.

--- libft/libft.h
+++ libft/libft.h
@@ -35,6 +35,12 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **alst, t_list *new);
+void	ft_lstadd_front(t_list **alst, t_list *new);
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+
 // External functions: write
 
 void	ft_putchar_fd(char c, int fd);
@@ -56,16 +62,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	**ft_split(char const *s, char c);
 void	*ft_calloc(size_t count, size_t size);
 
-// Linked list functions
-
-int		ft_lstsize(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
-t_list	*ft_lstlast(t_list *lst);
-void	ft_lstadd_back(t_list **alst, t_list *new);
-void	ft_lstadd_front(t_list **alst, t_list *new);
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
-void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
