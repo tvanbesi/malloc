ca22d53e3daea8f0796272c00851904cd10635e8
tvanbesi <tvanbesi@proton.me>
2022-09-12 19:13:17 +0200

    Add realloc() implementation

--- src/malloc.c
+++ src/malloc.c
@@ -21,7 +21,25 @@ void *malloc(size_t size)
 
 void *realloc(void *ptr, size_t size)
 {
-	(void)ptr;
-	(void)size;
-	return NULL;
+	void	*new_ptr;
+	size_t	ptr_original_size;
+
+	ptr_original_size = ((t_memory_pointer*)ptr)->size;
+	if (get_type_by_size(ptr_original_size) >= get_type_by_size(size))
+	{
+		alloc_pointer(ptr, size);
+		return ptr;
+	}
+	new_ptr = malloc(size);
+	if (new_ptr == NULL)
+	{
+		return NULL;
+	}
+	ft_memcpy(
+		(char*)new_ptr + sizeof(t_memory_pointer),
+		(char*)ptr + sizeof(t_memory_pointer),
+		ptr_original_size - sizeof(t_memory_pointer)
+		);
+	free(ptr);
+	return new_ptr;
 }
